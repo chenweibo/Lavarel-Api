874c42cba513e8249ea3fab49215c2e1dbfa81f0
chenweibo <563960993@qq.com>
2018-09-12 16:28:53 +0800

    Permission

---
+++ app/Api/Controllers/PermissionContorller.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Api\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class PermissionContorller extends BaseController
+{
+}
