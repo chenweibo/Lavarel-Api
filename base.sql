da967b836a7dd34480482bd5875eea0ab8e53ef9
chenweibo <563960993@qq.com>
2018-09-26 16:53:14 +0800

    update

--- base.sql
+++ base.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 100130
 File Encoding         : 65001
 
-Date: 2018-09-25 17:00:28
+Date: 2018-09-26 16:51:06
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -79,31 +79,35 @@ CREATE TABLE `navigations` (
   `name` varchar(255) DEFAULT NULL,
   `meta` text,
   `pid` int(255) DEFAULT NULL,
+  `hidden` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of navigations
 -- ----------------------------
-INSERT INTO `navigations` VALUES ('1', '/website', 'Layout', 'noredirect', 'website', '{\"title\":\"站点设置\",\"icon\":\"setup\",\"roles\":[\"editor\"]}', '0');
-INSERT INTO `navigations` VALUES ('2', '/baseSets', 'BaseSets', '#', 'BaseSets', '{\"title\":\"基础设置\",\"icon\":\"set\"}', '1');
-INSERT INTO `navigations` VALUES ('3', 'tree', '', '#', 'Tree', '{\"title\":\"Tree\",\"icon\":\"tree\",\"roles\":[\"editor\"]}', '1');
-INSERT INTO `navigations` VALUES ('4', '/users', '', '', '', '{\"title\":\"用户管理\",\"icon\":\"user\"}', '0');
-INSERT INTO `navigations` VALUES ('5', 'permissiom', '', '', 'Permission', '{\"title\":\"权限管理\",\"icon\":\"lock\"}', '0');
-INSERT INTO `navigations` VALUES ('6', '/gbook', '', null, 'Gbook', '{\"title\":\"留言管理\",\"icon\":\"gbook\"}', '0');
-INSERT INTO `navigations` VALUES ('7', '/page', '', null, 'Page', '{\"title\":\"单页管理\",\"icon\":\"page\"}', '0');
-INSERT INTO `navigations` VALUES ('8', '/article', null, null, 'Article', '{\"title\":\"文章管理\",\"icon\":\"article\"}', '0');
-INSERT INTO `navigations` VALUES ('9', '/project', null, null, null, '{\"title\":\"产品管理\",\"icon\":\"project\"}', '0');
-INSERT INTO `navigations` VALUES ('10', '/file', null, null, null, '{\"title\":\"文件系统\",\"icon\":\"file\"}', '0');
-INSERT INTO `navigations` VALUES ('11', '/wechat', null, null, null, '{\"title\":\"微信管理\",\"icon\":\"wechat\"}', '0');
-INSERT INTO `navigations` VALUES ('12', '/system', null, null, null, '{\"title\":\"系统设置\",\"icon\":\"system\"}', '0');
-INSERT INTO `navigations` VALUES ('13', 'other', null, null, null, '{\"title\":\"其他设置\",\"icon\":\"other\"}', '0');
-INSERT INTO `navigations` VALUES ('14', 'pay', null, null, null, '{\"title\":\"支付系统\",\"icon\":\"pay\"}', '0');
-INSERT INTO `navigations` VALUES ('15', '/user/list', 'User', null, 'User', '{\"title\":\"用户列表\",\"icon\":\"adduser\"}', '4');
-INSERT INTO `navigations` VALUES ('16', '/user/info', 'Person', null, 'Person', '{\"title\":\"个人中心\",\"icon\":\"person\"}', '4');
-INSERT INTO `navigations` VALUES ('17', '/permissiom/list', 'Permissionlist', null, 'Permissionlist', '{\"title\":\"角色管理\",\"icon\":\"permission\"}', '5');
-INSERT INTO `navigations` VALUES ('18', '/permissiom/role', 'Roles', 'Roles', 'Roles', '{\"title\":\"权限管理\",\"icon\":\"role\"}', '5');
-INSERT INTO `navigations` VALUES ('19', 'message', 'Message', 'Message', 'Message', '{\"title\":\"留言列表\",\"icon\":\"message\"}', '6');
+INSERT INTO `navigations` VALUES ('1', '/website', 'Layout', 'noredirect', 'website', '{\"title\":\"站点设置\",\"icon\":\"setup\",\"roles\":[\"editor\"]}', '0', null);
+INSERT INTO `navigations` VALUES ('2', '/baseSets', 'BaseSets', '#', 'BaseSets', '{\"title\":\"基础设置\",\"icon\":\"set\"}', '1', null);
+INSERT INTO `navigations` VALUES ('3', 'tree', '', '#', 'Tree', '{\"title\":\"Tree\",\"icon\":\"tree\",\"roles\":[\"editor\"]}', '1', null);
+INSERT INTO `navigations` VALUES ('4', '/users', '', '', '', '{\"title\":\"用户管理\",\"icon\":\"user\"}', '0', null);
+INSERT INTO `navigations` VALUES ('5', 'permissiom', '', '', 'Permission', '{\"title\":\"权限管理\",\"icon\":\"lock\"}', '0', null);
+INSERT INTO `navigations` VALUES ('6', '/gbook', '', null, 'Gbook', '{\"title\":\"留言管理\",\"icon\":\"gbook\"}', '0', null);
+INSERT INTO `navigations` VALUES ('7', '/page', '', null, 'Page', '{\"title\":\"单页管理\",\"icon\":\"page\"}', '0', null);
+INSERT INTO `navigations` VALUES ('8', '/article', null, null, 'Article', '{\"title\":\"文章管理\",\"icon\":\"article\"}', '0', null);
+INSERT INTO `navigations` VALUES ('9', '/project', null, null, null, '{\"title\":\"产品管理\",\"icon\":\"project\"}', '0', null);
+INSERT INTO `navigations` VALUES ('10', '/file', null, null, null, '{\"title\":\"文件系统\",\"icon\":\"file\"}', '0', null);
+INSERT INTO `navigations` VALUES ('11', '/wechat', null, null, null, '{\"title\":\"微信管理\",\"icon\":\"wechat\"}', '0', null);
+INSERT INTO `navigations` VALUES ('12', '/system', null, null, null, '{\"title\":\"系统设置\",\"icon\":\"system\"}', '0', null);
+INSERT INTO `navigations` VALUES ('13', 'other', null, null, null, '{\"title\":\"其他设置\",\"icon\":\"other\"}', '0', null);
+INSERT INTO `navigations` VALUES ('14', 'pay', null, null, null, '{\"title\":\"支付系统\",\"icon\":\"pay\"}', '0', null);
+INSERT INTO `navigations` VALUES ('15', '/user/list', 'User', null, 'User', '{\"title\":\"用户列表\",\"icon\":\"adduser\"}', '4', null);
+INSERT INTO `navigations` VALUES ('16', '/user/info', 'Person', null, 'Person', '{\"title\":\"个人中心\",\"icon\":\"person\"}', '4', null);
+INSERT INTO `navigations` VALUES ('17', '/permissiom/list', 'Permissionlist', null, 'Permissionlist', '{\"title\":\"角色管理\",\"icon\":\"permission\"}', '5', null);
+INSERT INTO `navigations` VALUES ('18', '/permissiom/role', 'Roles', 'Roles', 'Roles', '{\"title\":\"权限管理\",\"icon\":\"role\"}', '5', null);
+INSERT INTO `navigations` VALUES ('19', 'message', 'Message', 'Message', 'Message', '{\"title\":\"留言列表\",\"icon\":\"message\"}', '6', null);
+INSERT INTO `navigations` VALUES ('20', 'Pagelist', null, 'page/list', 'Page', '{\"title\":\"单篇列表\",\"icon\":\"page\"}', '7', null);
+INSERT INTO `navigations` VALUES ('21', 'create', null, null, 'PageCreate', '{\"title\":\"单篇添加\",\"icon\":\"page\"}', '7', 'true');
+INSERT INTO `navigations` VALUES ('22', 'edit/:id(\\\\d+)', null, null, 'PageEdit', '{\"title\":\"单篇编辑\",\"icon\":\"page\"}', '7', 'true');
 
 -- ----------------------------
 -- Table structure for password_resets
