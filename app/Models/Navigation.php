584c4563c92a85ed37613fc53731154ef6fe1875
chenweibo <563960993@qq.com>
2018-10-14 16:05:22 +0800

    add Laravel IDE Helper

--- app/Models/Navigation.php
+++ app/Models/Navigation.php
@@ -4,6 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Navigation
+ *
+ * @property int $id
+ * @property string|null $path
+ * @property string|null $component
+ * @property string|null $redirect
+ * @property string|null $name
+ * @property string|null $meta
+ * @property int|null $pid
+ * @property string|null $hidden
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Navigation whereComponent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Navigation whereHidden($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Navigation whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Navigation whereMeta($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Navigation whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Navigation wherePath($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Navigation wherePid($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Navigation whereRedirect($value)
+ * @mixin \Eloquent
+ */
 class Navigation extends Model
 {
     //
--- app/Models/Templete.php
+++ app/Models/Templete.php
@@ -4,6 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Templete
+ *
+ * @mixin \Eloquent
+ */
 class Templete extends Model
 {
     //
