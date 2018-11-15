2ea02efe7a842efb6610915c8903f4f54539bd12
chenweibo <563960993@qq.com>
2018-11-15 16:46:05 +0800

    php-cs-fixer

--- app/Http/Controllers/Api/Admin/Controllers/PermissionContorller.php
+++ app/Http/Controllers/Api/Admin/Controllers/PermissionContorller.php
@@ -7,24 +7,26 @@ use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
 use App\Models\Navigation;
 
-
 class PermissionContorller extends BaseController
 {
     public function demo()
     {
         $data = changeMeta(Navigation::get()->toArray());
-        $nav =make_tree($data);
-        return  response()->json(['data'=>$nav,'demo'=>$nav[0]['meta']]);
+        $nav = make_tree($data);
+
+        return response()->json(['data' => $nav, 'demo' => $nav[0]['meta']]);
     }
+
     /****
      * 创建角色
      * @return \Illuminate\Http\JsonResponse
-     */
-     public function createRole (Request $request){
-         $role = Role::create(['name' => $request->name]);
-         $permission = Permission::create(['name' => 'create']);
-         $permission = Permission::create(['name' => 'edit']);
-         return  response()->json(['code'=>200]);
-     }
+     ****/
+    public function createRole(Request $request)
+    {
+        $role = Role::create(['name' => $request->name]);
+        $permission = Permission::create(['name' => 'create']);
+        $permission = Permission::create(['name' => 'edit']);
 
+        return response()->json(['code' => 200]);
+    }
 }
