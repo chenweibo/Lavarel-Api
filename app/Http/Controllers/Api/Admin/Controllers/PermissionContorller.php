ada81ae3ee15325dd68c1dfa92f333591aecd031
chenweibo <563960993@qq.com>
2018-11-23 15:45:05 +0800

    text function

--- app/Http/Controllers/Api/Admin/Controllers/PermissionContorller.php
+++ app/Http/Controllers/Api/Admin/Controllers/PermissionContorller.php
@@ -6,15 +6,28 @@ use Illuminate\Http\Request;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
 use App\Models\Navigation;
+use JWTAuth;
+use App\Models\User;
 
 class PermissionContorller extends BaseController
 {
     public function demo()
     {
-        $data = changeMeta(Navigation::get()->toArray());
-        $nav = make_tree($data);
+        //$data = changeMeta(Navigation::get()->toArray());
+        //$nav = make_tree($data);
+       $user = JWTAuth::parseToken()->authenticate(); //获取用户信息
+        //$user =User::find(2);
+        // $user->assignRole('普通管理员');
+         //$user->assignRole('writer');
+        //$user = User::permission('edit articles')->get();
+        //$user->hasRole('writer');
+        //$role = Role::findById(2);
+        //$role->givePermissionTo('edit articles');
+        // $d=  $user->can('edit articles');
+        // $roles = $user->getRoleNames(); // 返回一个集合
+        //$permissions = User::permission('edit articles')->get();
 
-        return response()->json(['data' => $nav, 'demo' => $nav[0]['meta']]);
+        return response()->json(['user'=>$user->getPermissionsViaRoles(),'d'=>$d]);
     }
 
     /****
